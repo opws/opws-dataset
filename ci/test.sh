353c2fcbe96979f365cb07ceca527c51a511cf06
Stuart P. Bentley <stuart@testtrack4.com>
2017-03-03 08:49:32 +0000

    Remove reduced-test trickery for CI (closes #123)

--- ci/test.sh
+++ ci/test.sh
@@ -4,17 +4,5 @@ set -eo pipefail
 CANDIDATE_BRANCH="$CIRCLE_BRANCH"
 CANDIDATE_HASH="$CIRCLE_SHA1"
 
-if [[ "$CANDIDATE_BRANCH" == "master" ]]; then
-  # Always run full validation on merges to master, just to allay all doubts
-  opws-validate profiles/* legacies/*
-
-else # For pull requests etc
-# Get the names of all added or modified files
-# From this hash to refs/heads/master
-# under the "profiles" and "legacies" directories
-# and run the validator on them
-git diff-tree --no-commit-id --name-only --no-renames --diff-filter=AM -r \
-  "$CANDIDATE_HASH" master \
-  profiles legacies | \
-  xargs --no-run-if-empty opws-validate
-fi
+# Always validate everything in CI
+opws-validate profiles/* legacies/*
